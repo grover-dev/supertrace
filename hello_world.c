02569c94fd38db30c78a776d1db0d5ea1dbf1e78
tim <timkouzmenkov@gmail.com>
2022-03-23 18:54:11 -0400

    normalization, starting vector calc do ne

--- hello_world.c
+++ hello_world.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define STEP 1.0
-
-#define BOUNDS 256
 
 
+#define BOUNDS 11
 
 // structure holding ray parameters
 struct ray_t{
@@ -83,22 +81,53 @@ void image_write(struct image_t * image){
     (void) fclose(fp);
 }
 
+void print_vector_to_console(void * vector, int n, int m){
 
 
-#define D 10.0
-#define PI 3.14159265
-#define THETA (PI/2)
+} 
+
+
+
+#define STEP 1.0
+
+
+float D = 10.0;
+const float PI = 3.14159265;
+float THETA = PI/2;
 
 int main(){
 //    cuda_hello<<<1,1>>>();
     struct ray_t ** rays = malloc(BOUNDS*BOUNDS*sizeof(struct ray_t *));
-    // creating BOUNDS x BOUNDS grid of rays
-    for(int x = 0; x < BOUNDS; x++){
-        for (int y = 0; y < BOUNDS; y++){
-                        
+    // calculating bounding coords
+    float start_x = - D * tan(THETA/2);
+    float start_y = D * tan(THETA/2);
+    float start_z = D;
+    //float delta_theta = THETA / (float)BOUNDS;
+    float delta = D * tan(THETA/2) * 2.0 / ((float)BOUNDS-1.0);
+    //printf("theta: %f, D: %f, x size: %f\n", THETA, D, size_x); 
+    
+    float x = start_x;
+    float y = start_y;
+    float z = start_z;
+    // creating BOUNDS x BOUNDS grid of rays 
+    for(int j = 0; j < BOUNDS; j++){
+        x = start_x;
+        for (int i = 0; i < BOUNDS; i++){
+            rays[i+j*BOUNDS] = new_ray(x, y, z); 
+            printf("(%.2f,%.2f)", rays[i+j*BOUNDS]->x, rays[i+j*BOUNDS]->y); 
+            x += delta;
         }
-    }
-     
+        y -= delta;
+        printf("\n");
 
+    }
+    
+    
+
+    // cleaning up
+    for (int i = 0; i < BOUNDS*BOUNDS;i++){
+        free(rays[i]);   
+    }
+    free(rays);
     return 0;
 }
