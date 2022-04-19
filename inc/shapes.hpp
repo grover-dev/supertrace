1d39e8710a3469c3964732e075390b4fd7bd61ea
Steve Jayne <stevejayne@Steves-MacBook-Air-2.local>
2022-04-18 21:32:55 -0400

    Defined for both host and device

--- inc/shapes.hpp
+++ inc/shapes.hpp
@@ -8,17 +8,17 @@
 
 struct Vec3 {
   float x,y,z;
-  __device__ Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
-  __device__ Vec3(const Vec3& v): x(v.x), y(v.y), z(v.z) {}
-  __device__ Vec3 operator + (const Vec3& v) const { return Vec3(x+v.x, y+v.y, z+v.z); }
-  __device__ Vec3 operator - (const Vec3& v) const { return Vec3(x-v.x, y-v.y, z-v.z); }
-  __device__ Vec3 operator * (float d) const { return Vec3(x*d, y*d, z*d); }
-  __device__ Vec3 operator / (float d) const { return Vec3(x/d, y/d, z/d); }
-  __device__ Vec3 max(const Vec3& v)
+  __device__ __host__ Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+  __device__ __host__ Vec3(const Vec3& v): x(v.x), y(v.y), z(v.z) {}
+  __device__ __host__ Vec3 operator + (const Vec3& v) const { return Vec3(x+v.x, y+v.y, z+v.z); }
+  __device__ __host__ Vec3 operator - (const Vec3& v) const { return Vec3(x-v.x, y-v.y, z-v.z); }
+  __device__ __host__ Vec3 operator * (float d) const { return Vec3(x*d, y*d, z*d); }
+  __device__ __host__ Vec3 operator / (float d) const { return Vec3(x/d, y/d, z/d); }
+  __device__ __host__ Vec3 max(const Vec3& v)
   { 
     return Vec3((x>v.x) ? x : v.x,(y>v.y) ? y : v.y,(z>v.z) ? z : v.z);
   }
-  __device__ Vec3 normalize() const {
+  __device__ __host__ Vec3 normalize() const {
     double mg = sqrt(x*x + y*y + z*z);
     return Vec3(x/mg,y/mg,z/mg);
   }
